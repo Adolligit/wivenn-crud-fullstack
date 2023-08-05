611e7255a23cbdb747e2ee5c7ec94ff46ffa18db
Adolligit <adelson.onod@gmail.com>
2023-08-05 01:41:51 -0300

    feat: Criando as 'migrations' e 'models' das entidades 'Employee', 'Department' e 'Task'

---
+++ back/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('controller', Controller::class);
+});
