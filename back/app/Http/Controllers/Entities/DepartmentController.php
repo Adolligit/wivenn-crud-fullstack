142a05231f6eaee5b644f62d2e0c51da8ab3a385
Adolligit <adelson.onod@gmail.com>
2023-08-05 09:46:59 -0300

    feat [routes]: (Department) CRUD. inicial finalizado

--- back/app/Http/Controllers/Entities/DepartmentController.php
+++ back/app/Http/Controllers/Entities/DepartmentController.php
@@ -3,12 +3,52 @@
 namespace App\Http\Controllers\Entities;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\DepartmentRequest;
+use App\Models\DepartmentModel;
+use Illuminate\Http\Response;
 
 class DepartmentController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return $request;
+        return DepartmentModel::all();
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(DepartmentRequest $request)
+    {
+        DepartmentModel::create($request->validated());
+        return response()->json($request, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DepartmentModel $department)
+    {
+        return $department;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(DepartmentRequest $request, DepartmentModel $department)
+    {
+        $department->update($request->validated());
+        return response()->json($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DepartmentModel $department)
+    {
+        $department->deleteOrFail();
+        return response('',Response::HTTP_NO_CONTENT);
     }
 }
