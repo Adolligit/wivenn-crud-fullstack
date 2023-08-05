66af6d04751a84ffaa0372423abac111506a1937
Adolligit <adelson.onod@gmail.com>
2023-08-05 05:47:40 -0300

    feat [routes]: Organizando rotas para as entidades

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,14 +1,9 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['prefix' => 'v1'], function () {
-    Route::apiResource('controller', Controller::class);
+Route::group(['prefix' => 'v1'], function() {
+    Route::group(['prefix' => 'department'], base_path('routes/v1/DepartmentRoutes.php'));
+    Route::group(['prefix' => 'employee'], base_path('routes/v1/EmployeeRoutes.php'));
+    Route::group(['prefix' => 'task'], base_path('routes/v1/TaskRoutes.php'));
 });
